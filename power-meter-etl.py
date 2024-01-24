ba6cf3e80c453a21967ff2f195f39efacee521c2
jgreenawalt-stronghold <jgreenawalt@strongholddm.com>
2024-01-24 08:56:14 -0500

    hourly values

--- power-meter-etl.py
+++ power-meter-etl.py
@@ -1,6 +1,6 @@
-import json
 import urllib3
 import requests
+from datetime import date, timedelta
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -26,21 +26,23 @@ pidata = {
 pi_user = "piwebapi_write"
 pi_password = "nNRf7USFQ6ca&1"
 
-start_time = "2023-11-27T00:00:00Z"
-end_time = "2023-11-27T00:59:59Z"
-interval = "1h"
+
+interval = "1m"
+today = date.today()
+yesterday = today - timedelta(days=1)
+yesterday.strftime("%Y-%m-%d")
 
 
 pi_headers = {"Content-type": "application/json", "X-Requested-With": "XmlHttpRequest"}
 pi_url_prefix = "https://sg-piintegrator.scrubgrass.com/piwebapi/streams/"
-pi_url_body = f"/summary/?summaryType=Average&startTime={start_time}&endTime={end_time}&interval={interval}"
+
 
 def get_hourly_value(path, session):
     try:
         response = session.get(path, headers=pi_headers, verify=False)
         if response.status_code == 200:
             r = response.json()
-            print(round(r["Items"][0]["Value"]["Value"], 3))
+            print(format(round(r["Items"][0]["Value"]["Value"], 3), ".3f"))
     except Exception as e:
         print(f'Error: {e}')
 
@@ -51,18 +53,27 @@ def get_web_id(tag, data):
             return point["webid"]
     return None
 
-tag_search = "MAJT402"
+tag_search = "PARASITE"
 web_id =  get_web_id(tag_search, pidata)
-path = pi_url_prefix + web_id + pi_url_body
+
 
 session = requests.Session()
 session.auth = (pi_user, pi_password)
 
-get_hourly_value(path, session)
+def get_daily_values(web_id):
+    for i in range(23):
+        start_time = f"{yesterday}T{str(i).zfill(2)}:00:00Z"
+        end_time = f"{yesterday}T{str(i+1).zfill(2)}:00:00Z"
+        pi_url_body = f"/summary/?summaryType=Average&startTime={start_time}&endTime={end_time}&interval={interval}" 
+        path = pi_url_prefix + web_id + pi_url_body
+        print(path)
+        print(start_time, end_time)
+        get_hourly_value(path, session)
 
+
+get_daily_values(get_web_id("MAJT402", pidata))
 # Format the data into XML
-#timestamp = dt.datetime.now()
-#timestamp.strftime("%Y-%m-%dT%H:%M:%S")
+
 
 # Upload the data using PJM Power Meter CLI -> Java executable
 
